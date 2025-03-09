cedd9850d4ed40f5f266ab795a99e1708fd418cc
Ajid8181 <120887418+Ajid8181@users.noreply.github.com>
2025-03-09 14:27:50 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("orderForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var username = document.getElementById("username").value;
-    var paket = document.getElementById("paket").value;
-    var message = `Halo, saya ingin memesan paket ${paket} untuk username: ${username}.`;
-    var whatsappURL = `https://wa.me/6282245224101?text=${encodeURIComponent(message)}`;
-    window.open(whatsappURL, "_blank");
-});
