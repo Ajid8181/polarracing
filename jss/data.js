d781a6563a84264245a22f7397ed41d60a2107ef
Ajid8181 <120887418+Ajid8181@users.noreply.github.com>
2026-01-19 15:48:38 +0700

    Delete jss directory

--- jss/data.js
+++
@@ -1,4 +0,0 @@
-const SERVICES=[
-{name:'Joki Rank',price:'25K'},
-{name:'Top Up Coin',price:'10K'}
-];
--- jss/store.js
+++
@@ -1,7 +0,0 @@
-const store=document.getElementById('store');
-SERVICES.forEach(s=>{
-const el=document.createElement('div');
-el.className='card';
-el.innerHTML=`<h3>${s.name}</h3><strong>${s.price}</strong>`;
-store.appendChild(el);
-});
